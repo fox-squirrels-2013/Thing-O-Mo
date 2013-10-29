fbdb000e6bbf75392e0887e66ec6cc36be4d70d1
Mohit Ramchandani <mohitr35@gmail.com>
2013-10-28 17:21:11 -0700

    added create link

--- app.rb
+++ app.rb
@@ -5,10 +5,14 @@ require_relative 'app/models/game'
 ActiveRecord::Base.establish_connection(adapter: 'postgresql', database: 'sinatra_skeleton_dev')
 
 get '/' do
-	# @name = Game.
+	@name = Game.name
   erb :index
 end
 
 get '/create' do
 	erb :create
+end
+
+post '/create' do
+	Game.create(:name => ("#{params[:name]}"))
 end
--- db/migrate/20131028031000_create_games.rb
+++ db/migrate/20131028031000_create_games.rb
@@ -1,6 +1,6 @@
 class CreateGames < ActiveRecord::Migration
 	def change
-		create_table(:games) do |t|
+		create_table :games do |t|
 			t.string :name
 		end
 	end
